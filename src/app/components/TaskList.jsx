8734b0a039d799217bb81ce1a43a2421af871e63
spencerkingman-niche <skingman@niche.com>
2019-08-21 12:05:22 -0400

    Working on the app

---
+++ src/app/components/TaskList.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {
+    connect
+} from 'react-redux'
+
+export const TaskList = ({ tasks, name }) => {
+    return ( 
+        <div>
+        <h3>{name}</h3>
+        <div> 
+            {
+                tasks.map( task => (
+                    <div 
+                        key = { task.name }
+                    > 
+                        { task.name } 
+                    </div>
+                ))
+            }
+        </div>
+        </div>
+    )
+}
+
+function mapStateToProps(state, ownProps) {
+    let groupID = ownProps.id
+    return {
+        name: ownProps.name,
+        id: groupID,
+        tasks: state.tasks.filter(task=>task.group === groupID)
+    }
+}
+
+export const ConnectedTaskList = connect(mapStateToProps)(TaskList)
